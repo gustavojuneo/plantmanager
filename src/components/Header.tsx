adcb87fab33d679c3321dac6086c8dadc4497091
Gustavo Rodrigues <gustavo.rodrigues@linearsistemas.com.br>
2021-04-21 10:13:59 -0300

    create plants page & lists

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import userImg from '../assets/gustavo.jpeg'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Gustavo</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight()
+  },
+
+  greeting: {
+    fontSize: 32,
+    fontFamily: fonts.text,
+    color: colors.heading
+  },
+
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+    color: colors.heading
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  }
+})
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
 
 import { SafeAreaView } from '../components/SafeAreaView'
 import { Button } from '../components/Button'
@@ -8,6 +9,12 @@ import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 export function Confirmation() {
+  const navigation = useNavigation()
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect')
+  }
+
   return (
     <SafeAreaView>
       <View style={styles.content}>
@@ -21,7 +28,7 @@ export function Confirmation() {
         </Text>
 
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
